08f20b3897980fde4f0355a32bf2c9f31fa221f9
Griffin <griffincriste@gmail.com>
2020-06-18 21:46:40 -0500

    fixed border shadow on dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import Title from './Title';
 import Wrapper from './Wrapper';
 import Footer from './Footer';
 import add from '../images/addicon.png';
@@ -13,61 +12,32 @@ class Dashboard extends Component {
 		return (
 			<div>
 				<Wrapper>
-					<Title>Welcome!</Title>
 					<div className='container'>
+						<h1 style={{ textAlign: 'center' }}>Welcome!</h1>
+						<h6 style={{ textAlign: 'center' }}>Click on one of the buttons below to get started!</h6>
 						<div className='card'>
 							<div className='row'>
 								<div className='col-md-3'>
 									<Link to='/add'>
-										<img className='picture' src={add} alt='pic of math' width='255' height='255' />
+										<img className='picture' src={add} alt='pic of math' />
 									</Link>
 								</div>
 								<div className='col-md-3'>
 									<Link to='/subtract'>
-										<img
-											className='picture'
-											src={subtract}
-											alt='pic of math'
-											width='255'
-											height='255'
-										/>
+										<img className='picture' src={subtract} alt='pic of math' />
 									</Link>
 								</div>
 								<div className='col-md-3'>
 									<Link to='/multiply'>
-										<img
-											className='picture'
-											src={multiply}
-											alt='pic of math'
-											width='255'
-											height='255'
-										/>
+										<img className='picture' src={multiply} alt='pic of math' />
 									</Link>
 								</div>
 								<div className='col-md-3'>
 									<Link to='/divide'>
-										<img
-											className='picture'
-											src={divide}
-											alt='pic of math'
-											width='255'
-											height='255'
-										/>
+										<img className='picture' src={divide} alt='pic of math' />
 									</Link>
 								</div>
-
-								<div className='col-md-4 offset-md-4'>
-									{/* <Link to={'/add'}>
-															<button className='ui yellow horizontal animated button'>
-																<div className='visible content'>Back to Login</div>
-																<div className='hidden content'>
-																	<i className='left arrow icon' />
-																</div>
-															</button>
-														</Link> */}
-								</div>
 							</div>
-							<div className='row' />
 						</div>
 					</div>
 				</Wrapper>
--- src/components/Title.js
+++ src/components/Title.js
@@ -3,8 +3,10 @@ import React from 'react';
 function Title(props) {
 	return (
 		<div>
-			<h1 className='title'>{props.children}</h1>
-			<h6 className='directions'>
+			<h1 className='title' style={{ textAlign: 'center' }}>
+				{props.children}
+			</h1>
+			<h6 className='directions' style={{ textAlign: 'center' }}>
 				{' '}
 				Type in the answer to the problem. If you get it right, you keep going. But if you get it wrong, you
 				lose!
